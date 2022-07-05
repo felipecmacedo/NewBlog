a1e71785cec35cb3a3f0f80087deb5708e13a2b2
Felipe Cimardi Macedo <felipecimardimacedo@gmail.com>
2022-07-04 21:22:40 -0300

    update project

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -8,14 +8,10 @@ namespace Blog.Data
     {
         public DbSet<Category> Categories { get; set; }
         public DbSet<Post> Posts { get; set; }
-        // public DbSet<PostTag> PostTags { get; set; }
-        // public DbSet<Role> Roles { get; set; }
-        // public DbSet<Tag> Tags { get; set; }
         public DbSet<User> Users { get; set; }
-        // public DbSet<UserRole> UserRoles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer("Server=localhost,1433;Database=BLog;User ID=sa;Password=Ghfkgurhg@8683");
+            => options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=Ghfkgurhg@8683");
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
